cf717286ad8e7c3bfee700ac20f62a5265d90c51
Method <method@CLT1-LMAC-74700.local>
2022-10-27 11:53:56 -0400

    Added instructions to work on project locally

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 // install git on windows: https://git-scm.com/download/win
 // vscode
-// github
+// githubh1
+// 1) set up git: https://git-scm.com/download/win (accept defaults) 
+    // verify that git is installed by running: git --version
+// 2) create a folder for your projects
+// 3) cd into that project
+// 4) git clone https://github.com/YousefAyesh/ProfileProject.git
+// 5) prompt for username and password (classic token)
